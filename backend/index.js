ee05af5a15751005cc2cea59804c2314d5ee3de5
Rahul Patel <rr710505@gmail.com>
2024-02-22 22:54:44 +0530

    fd

--- backend/index.js
+++ backend/index.js
@@ -8,39 +8,43 @@ const PORT = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // this cor help to send data our frontend to database
-app.use(cors())
+app.use(cors(
+  {
+    origin:["https://deploy-mern-1whq.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true,
+  }
+));
 // connect to database
-  // online data base add
-  const DB = "mongodb+srv://rr710505:Fo5KOGyuGSPI3TNi@cluster0.u67jqoi.mongodb.net/mernStack?retryWrites=true&w=majority"
+// online data base add
+const DB =
+  "mongodb+srv://rr710505:Fo5KOGyuGSPI3TNi@cluster0.u67jqoi.mongodb.net/mernStack?retryWrites=true&w=majority";
 
-mongoose.connect(DB, {
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }).then(() => {
+  })
+  .then(() => {
     console.log("Databse is connected");
-  }).catch((error) => {
+  })
+  .catch((error) => {
     console.log(error);
   });
 
 // mongodb://localhost:27017/KeeperApp
 
-
-
 app.get("/", (req, res) => {
   res.json({ msg: "this is property management app ruunning " });
 });
 
-
-
 // require our routes
-const auth_route = require("./routes/auth")
+const auth_route = require("./routes/auth");
 const notes_route = require("./routes/notes");
 // const { Db } = require("mongodb");
 
 // middleware
-app.use("/api" , auth_route,notes_route)
-
-
+app.use("/api", auth_route, notes_route);
 
 // start aur backend
 app.listen(PORT, () => {
